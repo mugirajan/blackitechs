9986766bcf29770235d5b09fe07121d1ea3f15af
mugilavathi <mugirajan95@gmail.com>
2023-12-08 15:41:05 +0530

    bits

---
+++ dist/assets/js/navigation.js
@@ -0,0 +1,34 @@
+//home page
+function redirectToGetStarted() {
+    window.location.href = 'career.html'; 
+}
+
+function redirectToKnowMore() {
+    window.location.href = 'company.html'; 
+}
+
+
+//services page
+function redirectToWebApp() {
+    window.location.href = 'web-app.html'; 
+}
+
+function redirectToCustomBuilt() {
+    window.location.href = 'custom.html'; 
+}
+
+function redirectToSoftware() {
+    window.location.href = 'software.html'; 
+}
+
+function redirectToShopify() {
+    window.location.href = 'web-app.html'; 
+}
+
+function redirectToWordPress() {
+    window.location.href = 'wb.html'; 
+}
+
+function redirectToMobileApp() {
+    window.location.href = 'mob-app.html'; 
+}
